c9710b3a4fdd4d9f4d3fc8946d39f4ed07bdd296
Marc Schwartz <marcschwartz1990@gmail.com>
2022-08-03 20:45:16 -0400

    Initial Commit

---
+++ CountTo100/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountTo100
+//
+//  Created by Marc-Developer on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountTo100/CountTo100App.swift
@@ -0,0 +1,17 @@
+//
+//  CountTo100App.swift
+//  CountTo100
+//
+//  Created by Marc-Developer on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CountTo100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
